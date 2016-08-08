96fcb8a16aa06ec0dd3900373da765519de05eb5
José Castelo <Jos Castelo>
2016-08-08 19:16:28 +0100

    Initial Commit.

---
+++ tempos.js
@@ -0,0 +1,53 @@
+URL = "http://tempos.min-saude.pt/api.php/standbyTime/211";
+INTERVALO_TEMPO = 3600/2 * 1000;
+
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var express = require('express');
+
+function parseData(){
+	request(URL, function(error, response, data){
+
+        if(!error){
+			data = JSON.parse(data);
+			for (i in data.Result) {
+				var logtext = "";
+				if (data.Result[i].Queue != null) {
+					var dados = dados = data.Result[i]
+					logtext += data.Result[i].Queue.Description + " >> ";
+					logtext += dados.LastUpdate+"\t";
+					logtext += " R-"+dados.Red.Time+"-"+dados.Red.Length+"\t";
+					logtext += " O-"+dados.Orange.Time+"-"+dados.Orange.Length+"\t";
+					logtext += " Y-"+dados.Yellow.Time+"-"+dados.Yellow.Length+"\t";
+					logtext += " G-"+dados.Green.Time+"-"+dados.Green.Length+"\t";
+					logtext += " B-"+dados.Blue.Time+"-"+dados.Blue.Length;
+					logtext += "\n"
+				} else {
+					var dados = dados = data.Result[i]
+					logtext += "Geral" + " >> ";
+					logtext += dados.LastUpdate+"\t";
+					logtext += " R-"+dados.Red.Time+"-"+dados.Red.Length+"\t";
+					logtext += " O-"+dados.Orange.Time+"-"+dados.Orange.Length+"\t";
+					logtext += " Y-"+dados.Yellow.Time+"-"+dados.Yellow.Length+"\t";
+					logtext += " G-"+dados.Green.Time+"-"+dados.Green.Length+"\t";
+					logtext += " B-"+dados.Blue.Time+"-"+dados.Blue.Length;
+					logtext += "\n"
+				}
+				fs.appendFile('registoTempos', logtext, function (err) {});
+			}
+        }
+    })
+}
+
+parseData();
+setInterval(parseData, INTERVALO_TEMPO);
+
+var app     = express();
+
+app.get('/', function(req, res){
+	res.download("registoTempos");
+})
+
+app.listen('8080')
+
