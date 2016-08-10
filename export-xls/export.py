936bda2aad53e680bd9095b0bac9e1d80c47c4a9
José Castelo <Jos Castelo>
2016-08-10 19:47:12 +0100

    Data model change

--- export-xls/export.py
+++ export-xls/export.py
@@ -24,7 +24,7 @@ print(data);
 for i in range(len(data[u'entries'])):
 	entry = data[u'entries'][i]
 	print(entry);
-	name = entry[u"emergency"][u'name'] + " - " + entry[u'queue'][u'name']
+	name = entry[u'emergency'][u'name'] + " - " + entry[u'queue'][u'name']
 	if name not in names:
 		names.append(name)
 
